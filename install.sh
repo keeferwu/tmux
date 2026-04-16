9870c614e9f55eb518d3b1b8dc525b0cbcb3fdfe
qifeng <qifengwu@nettech-global.com>
2026-04-16 08:59:32 +0800

    add plugin tmux-continuum

--- install.sh
+++ install.sh
@@ -25,12 +25,14 @@ fi
 
 # insall tmux plugins
 tmux_plugin=${PWD}"/plugins"
-if [ ! -d $tmux_plugin ]; then
-    mkdir -p $tmux_plugin
-    git clone https://github.com/tmux-plugins/tpm $tmux_plugin/tpm
-    git clone https://github.com/tmux-plugins/tmux-sensible $tmux_plugin/tmux-sensible
-    git clone https://github.com/tmux-plugins/tmux-resurrect $tmux_plugin/tmux-resurrect
-    git clone https://github.com/tmux-plugins/tmux-yank $tmux_plugin/tmux-yank
-    git clone https://github.com/nhdaly/tmux-better-mouse-mode $tmux_plugin/tmux-better-mouse-mode
-    git clone https://github.com/NHDaly/tmux-scroll-copy-mode $tmux_plugin/tmux-scroll-copy-mode
+if [ -d $tmux_plugin ]; then
+    rm -rf $tmux_plugin
 fi
+mkdir -p $tmux_plugin
+git clone https://github.com/tmux-plugins/tpm $tmux_plugin/tpm
+git clone https://github.com/tmux-plugins/tmux-yank $tmux_plugin/tmux-yank
+git clone https://github.com/tmux-plugins/tmux-sensible $tmux_plugin/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-resurrect $tmux_plugin/tmux-resurrect
+git clone https://github.com/tmux-plugins/tmux-continuum $tmux_plugin/tmux-continuum
+git clone https://github.com/nhdaly/tmux-better-mouse-mode $tmux_plugin/tmux-better-mouse-mode
+git clone https://github.com/NHDaly/tmux-scroll-copy-mode $tmux_plugin/tmux-scroll-copy-mode
